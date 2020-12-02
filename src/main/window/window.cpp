69e6e12451954a6dc03a7bba537277ee3d3126fe
Tom Trabue <tom.trabue@gmail.com>
2020-12-02 16:55:36 -0500

    Added some more Window constructors

--- src/main/window/window.cpp
+++ src/main/window/window.cpp
@@ -1,8 +1,34 @@
 #include "window.hpp"
 #include <cstdlib>
+#include <stdexcept>
 
-xcu::Window::Window() { this->win_ = newwin(0, 0, 0, 0); }
+// Pull in the custom xcurses namespace.
+using namespace xcu;
 
-xcu::Window::Window(const Window &win) { this->win_ = dupwin(win.win_); }
+Window::Window() { win_ = newwin(0, 0, 0, 0); }
 
-xcu::Window::~Window() { free(this->win_); }
+Window::Window(const Window &win) {
+  if (win.win_) {
+    win_ = dupwin(win.win_);
+  } else {
+    win_ = nullptr;
+  }
+}
+
+Window::Window(int num_lines, int num_cols, int begin_y, int begin_x) {
+  if (num_lines < 0 || num_cols < 0 || begin_y < 0 || begin_x < 0) {
+    throw new std::out_of_range(
+        "All arguments to Window() must not be negative");
+  }
+  win_ = newwin(num_lines, num_cols, begin_y, begin_x);
+}
+
+Window::~Window() { free(win_); }
+
+Window &Window::operator=(const Window &win) {
+  if (win_) {
+    free(win_);
+  }
+  win_ = win.win_;
+  return *this;
+}
--- src/main/window/window.hpp
+++ src/main/window/window.hpp
@@ -9,18 +9,48 @@
 namespace xcu {
 
 /**
- * An ncurses Window representing a two-dimensional array of characters to be
- * printed to the screen.
+ * Wrapper class for the ncurses WINDOW struct.
+ * Represents a two-dimensional array of characters to be printed to the
+ * screen.
  */
 class Window {
 public:
+  /**
+   * Default constructor; produces a Window that fills the entire screen.
+   */
   Window();
+
+  /**
+   * Copy constructor
+   */
   Window(const Window &win);
+
+  // Other constructors
+  /**
+   * Construct a Window at position (0,0) (top left corner of the screen) with
+   * the given number of lines and columns.
+   */
+  Window(int num_lines, int num_cols);
+  /**
+   * Construct a Window at position (begin_x,begin_y) with the given number of
+   * lines and columns.
+   */
+  Window(int num_lines, int num_cols, int begin_y, int begin_x);
+
+  /**
+   * Destructor
+   */
   ~Window();
 
+  /**
+   * Overloaded assignment operator
+   */
   Window &operator=(const Window &win);
 
 private:
+  /**
+   * Encapsulated ncurses WINDOW structure.
+   */
   WINDOW *win_;
 };
 
